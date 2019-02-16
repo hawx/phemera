af4c36416279003580ec2c4f582537370083bc35
Joshua Hawxwell <m@hawx.me>
2019-02-16 10:34:05 +0000

    Use indie-auth package

--- phemera.go
+++ phemera.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/antage/eventsource"
+	"github.com/gorilla/sessions"
+	"hawx.me/code/indieauth"
 	"hawx.me/code/mux"
 	"hawx.me/code/serve"
 
 	"hawx.me/code/phemera/assets"
 	database "hawx.me/code/phemera/db"
-	"hawx.me/code/phemera/indieauth"
 	"hawx.me/code/phemera/markdown"
 	"hawx.me/code/phemera/models"
 	"hawx.me/code/phemera/views"
@@ -27,6 +31,7 @@ type Conf struct {
 	Secret      string
 	Horizon     string
 	DbPath      string
+	Me          string
 }
 
 type Context struct {
@@ -113,27 +118,82 @@ func main() {
 		log.Fatal("toml:", err)
 	}
 
+	if conf.Me == "" {
+		log.Fatal("me must be set to sign-in")
+	}
+
+	auth, err := indieauth.Authentication(conf.URL, conf.URL+"/callback")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	endpoints, err := indieauth.FindEndpoints(conf.Me)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	db := database.Open(conf.DbPath, conf.Horizon)
 	defer db.Close()
 
 	es := eventsource.New(nil, nil)
 	defer es.Close()
 
-	store := indieauth.NewStore(conf.Secret)
+	store := NewStore(conf.Secret)
 
-	shield := func(h http.Handler) http.Handler {
-		return indieauth.Protect(store, h, http.NotFoundHandler())
+	protect := func(store *meStore, good, bad http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if addr := store.Get(r); addr == conf.Me {
+				good.ServeHTTP(w, r)
+			} else {
+				bad.ServeHTTP(w, r)
+			}
+		})
 	}
 
-	http.Handle("/", indieauth.Protect(store, List(conf, db, true), List(conf, db, false)))
-	http.Handle("/add", shield(Add(conf, db, es)))
-	http.Handle("/preview", shield(Preview))
+	http.Handle("/", protect(store, List(conf, db, true), List(conf, db, false)))
+	http.Handle("/add", protect(store, Add(conf, db, es), http.NotFoundHandler()))
+	http.Handle("/preview", protect(store, Preview, http.NotFoundHandler()))
 	http.Handle("/feed", Feed(conf, db))
 	http.Handle("/connect", es)
 
-	http.Handle("/sign-in", indieauth.SignIn())
-	http.Handle("/callback", indieauth.Callback(store))
-	http.Handle("/sign-out", indieauth.SignOut(store, "/"))
+	http.HandleFunc("/sign-in", func(w http.ResponseWriter, r *http.Request) {
+		state, err := store.SetState(w, r)
+		if err != nil {
+			http.Error(w, "could not start auth", http.StatusInternalServerError)
+			return
+		}
+
+		redirectURL := auth.RedirectURL(endpoints, conf.Me, state)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, redirectURL, http.StatusFound)
+	})
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		state := store.GetState(r)
+
+		if r.FormValue("state") != state {
+			http.Error(w, "state is bad", http.StatusBadRequest)
+			return
+		}
+
+		me, err := auth.Exchange(endpoints, r.FormValue("code"))
+		if err != nil || me != conf.Me {
+			http.Error(w, "nope", http.StatusForbidden)
+			return
+		}
+
+		store.Set(w, r, me)
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+
+	http.HandleFunc("/sign-out", func(w http.ResponseWriter, r *http.Request) {
+		store.Set(w, r, "")
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets.Server(map[string]string{
 		"jquery.caret.js":        assets.Caret,
@@ -143,4 +203,52 @@ func main() {
 	})))
 
 	serve.Serve(*port, *socket, Log(http.DefaultServeMux))
+}
+
+type meStore struct {
+	store sessions.Store
+}
+
+func NewStore(secret string) *meStore {
+	return &meStore{sessions.NewCookieStore([]byte(secret))}
+}
+
+func (s meStore) Get(r *http.Request) string {
+	session, _ := s.store.Get(r, "session")
+
+	if v, ok := session.Values["me"].(string); ok {
+		return v
+	}
+
+	return ""
+}
+
+func (s meStore) Set(w http.ResponseWriter, r *http.Request, me string) {
+	session, _ := s.store.Get(r, "session")
+	session.Values["me"] = me
+	session.Save(r, w)
+}
+
+func (s meStore) SetState(w http.ResponseWriter, r *http.Request) (string, error) {
+	bytes := make([]byte, 32)
+
+	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
+		return "", err
+	}
+
+	state := base64.StdEncoding.EncodeToString(bytes)
+
+	session, _ := s.store.Get(r, "session")
+	session.Values["state"] = state
+	return state, session.Save(r, w)
+}
+
+func (s meStore) GetState(r *http.Request) string {
+	session, _ := s.store.Get(r, "session")
+
+	if v, ok := session.Values["state"].(string); ok {
+		return v
+	}
+
+	return ""
 }
