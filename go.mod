b8c1a1b7af36b7b2554aeed14b71bb3d66c42304
Joshua Hawxwell <m@hawx.me>
2019-03-02 14:53:12 +0000

    Use indieauth/sessions

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/hoisie/mustache v0.0.0-20160804235033-6375acf62c69
 	github.com/russross/blackfriday v1.5.2
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	hawx.me/code/indieauth v0.0.0-20190217173404-03d7e4c53487
+	hawx.me/code/indieauth v0.0.0-20190302144758-4bab90bdd555
 	hawx.me/code/mux v0.0.0-20171229202905-76b5c781b7be
 	hawx.me/code/serve v0.0.0-20190207181551-eb94630184cf
 )
--- phemera.go
+++ phemera.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/antage/eventsource"
-	"github.com/gorilla/sessions"
 	"hawx.me/code/indieauth"
+	"hawx.me/code/indieauth/sessions"
 	"hawx.me/code/mux"
-	"hawx.me/code/serve"
-
 	"hawx.me/code/phemera/assets"
 	database "hawx.me/code/phemera/db"
 	"hawx.me/code/phemera/markdown"
 	"hawx.me/code/phemera/models"
 	"hawx.me/code/phemera/views"
+	"hawx.me/code/serve"
 )
 
 type Conf struct {
@@ -127,7 +123,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	endpoints, err := indieauth.FindEndpoints(conf.Me)
+	session, err := sessions.New(conf.Me, conf.Secret, auth)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -138,62 +134,15 @@ func main() {
 	es := eventsource.New(nil, nil)
 	defer es.Close()
 
-	store := NewStore(conf.Secret)
-
-	protect := func(store *meStore, good, bad http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if addr := store.Get(r); addr == conf.Me {
-				good.ServeHTTP(w, r)
-			} else {
-				bad.ServeHTTP(w, r)
-			}
-		})
-	}
-
-	http.Handle("/", protect(store, List(conf, db, true), List(conf, db, false)))
-	http.Handle("/add", protect(store, Add(conf, db, es), http.NotFoundHandler()))
-	http.Handle("/preview", protect(store, Preview, http.NotFoundHandler()))
+	http.Handle("/", session.Choose(List(conf, db, true), List(conf, db, false)))
+	http.Handle("/add", session.Shield(Add(conf, db, es)))
+	http.Handle("/preview", session.Shield(Preview))
 	http.Handle("/feed", Feed(conf, db))
 	http.Handle("/connect", es)
 
-	http.HandleFunc("/sign-in", func(w http.ResponseWriter, r *http.Request) {
-		state, err := store.SetState(w, r)
-		if err != nil {
-			http.Error(w, "could not start auth", http.StatusInternalServerError)
-			return
-		}
-
-		redirectURL := auth.RedirectURL(endpoints, conf.Me, state)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, redirectURL, http.StatusFound)
-	})
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		state := store.GetState(r)
-
-		if r.FormValue("state") != state {
-			http.Error(w, "state is bad", http.StatusBadRequest)
-			return
-		}
-
-		me, err := auth.Exchange(endpoints, r.FormValue("code"))
-		if err != nil || me != conf.Me {
-			http.Error(w, "nope", http.StatusForbidden)
-			return
-		}
-
-		store.Set(w, r, me)
-		http.Redirect(w, r, "/", http.StatusFound)
-	})
-
-	http.HandleFunc("/sign-out", func(w http.ResponseWriter, r *http.Request) {
-		store.Set(w, r, "")
-		http.Redirect(w, r, "/", http.StatusFound)
-	})
+	http.Handle("/sign-in", session.SignIn())
+	http.Handle("/callback", session.Callback())
+	http.Handle("/sign-out", session.SignOut())
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets.Server(map[string]string{
 		"jquery.caret.js":        assets.Caret,
@@ -203,52 +152,4 @@ func main() {
 	})))
 
 	serve.Serve(*port, *socket, Log(http.DefaultServeMux))
-}
-
-type meStore struct {
-	store sessions.Store
-}
-
-func NewStore(secret string) *meStore {
-	return &meStore{sessions.NewCookieStore([]byte(secret))}
-}
-
-func (s meStore) Get(r *http.Request) string {
-	session, _ := s.store.Get(r, "session")
-
-	if v, ok := session.Values["me"].(string); ok {
-		return v
-	}
-
-	return ""
-}
-
-func (s meStore) Set(w http.ResponseWriter, r *http.Request, me string) {
-	session, _ := s.store.Get(r, "session")
-	session.Values["me"] = me
-	session.Save(r, w)
-}
-
-func (s meStore) SetState(w http.ResponseWriter, r *http.Request) (string, error) {
-	bytes := make([]byte, 32)
-
-	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
-		return "", err
-	}
-
-	state := base64.StdEncoding.EncodeToString(bytes)
-
-	session, _ := s.store.Get(r, "session")
-	session.Values["state"] = state
-	return state, session.Save(r, w)
-}
-
-func (s meStore) GetState(r *http.Request) string {
-	session, _ := s.store.Get(r, "session")
-
-	if v, ok := session.Values["state"].(string); ok {
-		return v
-	}
-
-	return ""
 }
